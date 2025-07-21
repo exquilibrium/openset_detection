203ca023324b1b0c98f35e8170dc95040d18b365
Leonhard Chen <chen.leonhard@gmail.com>
2025-07-22 01:48:21 +0200

    Data Preparation Baseline

--- scripts/slurm/frcnn_lru1_all.sh
+++ scripts/slurm/frcnn_lru1_all.sh
@@ -11,7 +11,7 @@ PROJECT_ROOT="/home/chen/openset_detection/scripts/FRCNN"
 #bash "$PROJECT_ROOT/training/train_frcnn_lru1_lru2.sh"
 
 # Run testing script
-#bash "$PROJECT_ROOT/extraction/test_frcnn_lru1.sh"
-#bash "$PROJECT_ROOT/extraction/test_frcnn_lru1_drone.sh"
+bash "$PROJECT_ROOT/extraction/test_frcnn_lru1.sh"
+bash "$PROJECT_ROOT/extraction/test_frcnn_lru1_drone.sh"
 bash "$PROJECT_ROOT/extraction/test_frcnn_lru1_lander.sh"
-#bash "$PROJECT_ROOT/extraction/test_frcnn_lru1_lru2.sh"
+bash "$PROJECT_ROOT/extraction/test_frcnn_lru1_lru2.sh"
