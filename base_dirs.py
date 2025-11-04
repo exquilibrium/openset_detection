c313063a73c775fbd540f36beb395aec4ee6d4cb
Mr.Chen <chen.leonhard@gmail.com>
2025-11-04 13:41:17 +0100

    path adjustments

--- base_dirs.py
+++ base_dirs.py
@@ -7,6 +7,6 @@ if BASE_DIR_FOLDER == '/home/chen/openset_detection':
     BASE_VOC_FOLDER = '/media/chen/76AECF8EAECF4579/data'
 else: # '/home/chen_le/openset_detection' >>> DLR
     BASE_DATA_FOLDER = '/volume/hot_storage/slurm_data/chen_le/ARCHES'
-    BASE_VOC_FOLDER = '/volume/hot_storage/slurm_data/chen_le'
+    BASE_VOC_FOLDER = '/volume/hot_storage/slurm_data/chen_le/VOC'
 BASE_RESULTS_FOLDER = BASE_DIR_FOLDER + '/results'
 BASE_PRETRAINED_FOLDER = BASE_DIR_FOLDER + '/pretrained'
